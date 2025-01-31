429ec9a7f4253e0558eb4ae554ba726dd80f682b
alsomedroo <dhruvattek@gmail.com>
2025-01-31 09:18:33 +0530

    broadcast in different roomId done

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,42 @@ import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
-let allSockets: WebSocket[] = [];
+
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount++;
-    console.log(`User connected, total users: ${userCount}`);
-
+    
     socket.on("message", (message) => {
-        console.log("Message received -> " + message.toString())
-        for (let i = 0 ; i<allSockets.length ; i++) {
-            const s = allSockets[i];
-            s.send(message.toString()+ " : sent from the server");
+        const parsedMessage = JSON.parse(message as unknown as string);
+        if(parsedMessage.type==="join"){
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+        if (parsedMessage.type==="chat"){
+            let currentUserRoom = null;
+            for (let i = 0 ; i<allSockets.length ; i++) {
+                if (allSockets[i].socket===socket) {
+                    currentUserRoom = allSockets[i].room;
+                    break;
+                }
+            }
+            for (let i = 0 ; i<allSockets.length ; i++) {
+                if (allSockets[i].room===currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+
         }
         
-        
-       
     })
+
+    
 
 
 })
